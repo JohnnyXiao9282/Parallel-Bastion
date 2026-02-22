b70236ff8248da941ea4653a18b7b5d3cbcc5f68
Johnny Xiao <yuxiao9282@gmail.com>
2026-02-21 21:21:10 -0800

    first implementation of threadpool for tasks

--- src/concurrency/ThreadPool.cpp
+++ src/concurrency/ThreadPool.cpp
@@ -5,7 +5,24 @@ namespace Concurrency {
 ThreadPool::ThreadPool(size_t numThreads)
     : m_stop(false)
 {
-    (void)numThreads;
+    for (size_t i = 0; i < numThreads; ++i) {
+        m_workers.emplace_back([this]() {
+            while (true) {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(this->m_queueMutex);
+                    this->m_condition.wait(lock, [this]() {
+                        return this->m_stop || !this->m_tasks.empty();
+                    });
+                    if (this->m_stop && this->m_tasks.empty())
+                        return;
+                    task = std::move(this->m_tasks.front());
+                    this->m_tasks.pop();
+                }
+                task();
+            }
+        });
+    }
 }
 
 ThreadPool::~ThreadPool() {
--- src/concurrency/ThreadPool.h
+++ src/concurrency/ThreadPool.h
@@ -14,17 +14,30 @@ class ThreadPool {
 public:
     ThreadPool(size_t numThreads = std::thread::hardware_concurrency());
     ~ThreadPool();
-    
+
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
-    
+
     template<typename F, typename... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result<F, Args...>::type> {
+        using ReturnType = typename std::invoke_result<F, Args...>::type;
+        auto task = std::make_shared<std::packaged_task<ReturnType()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        std::future<ReturnType> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(m_queueMutex);
+            if (m_stop)
+                throw std::runtime_error("ThreadPool is stopped");
+            m_tasks.emplace([task]() { (*task)(); });
+        }
+        m_condition.notify_one();
+        return res;
+    }
     
 private:
     std::vector<std::thread> m_workers;
     std::queue<std::function<void()>> m_tasks;
-    
     std::mutex m_queueMutex;
     std::condition_variable m_condition;
     bool m_stop;
